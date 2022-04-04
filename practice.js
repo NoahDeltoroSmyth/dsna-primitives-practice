c4a69c1408622be517d8ecf191d08ca29f740ca3
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-04-04 11:12:27 -0700

    fix titlecase

--- practice.js
+++ practice.js
@@ -2,8 +2,17 @@ function titleCase(sentence) {
   return sentence
     .split(' ')
     .map((word) => {
-      return word[0].toLowerCase() + word.slice(1).toUpperCase();
+      return word[0].toUpperCase() + word.slice(1).toLowerCase();
     })
     .join(' ');
 }
 console.log(titleCase('Here is a sentence'));
+//
+//
+// const arr = ['a', 'b', 'c', 'd'];
+// const length = arr.length();
+// function at(arr, index) {
+//   // return arr.find().at(index[1]);
+//   return arr[index] > -1;
+// }
+// console.log(at(arr[1]));
